bc6b59763193b72b98f18b05555674350b6e988e
Alex Muller de Jesus Sousa <a_lexmiller@hotmail.com>
2018-04-26 18:50:05 -0300

    Update click.js

--- events/click.js
+++ events/click.js
@@ -1,10 +1,10 @@
-var EventClick = (function () {
+var ClickEvent = (function () {
 	/*
 	 * Cria um click event
 	 * 
 	 *  return Event
 	 */
-	return function EventClick(callback) {
+	return function ClickEvent(callback) {
 		if (!ALLOWED.event(GET.const('EVENT_CLICK_NAME'))) 
 			throw new Exception('CreateClickEventException', 'The event click can not be created!');
 		
